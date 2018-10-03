dc72610af2ad894b33daed6b7063f1cb56b413aa
natebecker11 <natebecker11@gmail.com>
2018-10-03 15:51:59 -0400

    finished spotify-this-song

--- liri.js
+++ liri.js
@@ -9,6 +9,7 @@ const keys = require('./keys.js')
 const spotify = new Spotify(keys.spotify);
 
 
+
 // concert-this function
 const concertSearch = (artist = 'LCD Soundsystem') => {
   console.log(`Looking up concerts for ${artist}, this may take a moment!`)
@@ -41,8 +42,42 @@ const concertSearch = (artist = 'LCD Soundsystem') => {
 }
 
 // spotify-this-song function
-const spotifySearch = (song = 'The Sign') => {
-  console.log('search a song')
+const spotifySearch = (song = 'The Sign Ace of Base') => {
+  // call the node-spotify-api module
+  spotify
+    // search for the given song
+    .search(
+      {
+        type: 'track',
+        query: song
+      },
+      (err, data) => {
+        // handle errors
+        if (err) {
+          return console.log(`Error occurred: ${err}`)
+        }
+        // bind the first result, could return multiple, however spotify tends to give a whole lot of irrelevant results
+        let firstResult = data['tracks']['items'][0];
+        // if no results are found, inform user and return
+        if (!firstResult) return console.log('No results found.')
+        // build a list of artists
+        let artists = firstResult['artists']
+          // grab the artist name from the object, add it on to the list
+          .reduce((list, artist) => {
+            return list += artist['name'] + ', '
+            },''
+          )
+          // remove the final comma and space from the string
+          .slice(0, -2)
+        // grab the song name and album
+        let songName = firstResult['name']
+        let album = firstResult['album']['name']        
+        // If a preview url is available, get that. Otherwise, get the full song url
+        let prevUrl = firstResult['preview_url'] ? 'Preview: ' + firstResult['preview_url'] : 'Listen here (requires account): ' + firstResult['external_urls']['spotify']
+        // display result to user
+        console.log(`Song: ${songName}\nArtist(s): ${artists}\nAlbum: ${album}\n${prevUrl}`)
+      }
+    )
 }
 
 // concert-this function
