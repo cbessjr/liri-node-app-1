72caf979a651f2f0eef626a48b2f54f718e74762
natebecker11 <natebecker11@gmail.com>
2018-10-03 14:33:43 -0400

    finished concert-this

--- liri.js
+++ liri.js
@@ -10,17 +10,42 @@ const spotify = new Spotify(keys.spotify);
 
 
 // concert-this function
-const concertSearch = () => {
-  return 'do a damned thing'
+const concertSearch = (artist = 'LCD Soundsystem') => {
+  console.log(`Looking up concerts for ${artist}, this may take a moment!`)
+  // call the bandsintown API
+  request('https://rest.bandsintown.com/artists/' + artist + '/events?app_id=codingbootcamp', (err, resp, body) => {
+    // check for success
+    if (!err && resp.statusCode === 200) {
+      console.log(!!JSON.parse(body)[0] ? 'Here are your concerts!' : `No concerts scheduled for ${artist} at this time.`)
+      // for each entry
+      JSON.parse(body).forEach(entry => {
+        // grab venue name, city/region/country
+        let name = entry['venue']['name']
+        let city = entry['venue']['city']
+        let region = entry['venue']['region']
+        let country = entry['venue']['country']
+        // grab date, format it
+        let date = moment(entry['datetime']).format('dddd, MMMM Do YYYY, h:mmA');
+        // if there's a region, log out this way
+        if (region) {
+          console.log('------\n', name+'\n', `${city}, ${region}, ${country}\n`, date)
+        }
+        // else log out this way
+        else {
+          console.log('------\n', name+'\n', `${city}, ${country}\n`, date)
+        }
+      })     
+    }
+  })
 }
 
 // spotify-this-song function
-const spotifySearch = () => {
+const spotifySearch = (song = 'The Sign') => {
   console.log('search a song')
 }
 
 // concert-this function
-const movieSearch = () => {
+const movieSearch = (movie = 'Mr. Nobody') => {
   console.log('search a movie')
 }
 
@@ -45,4 +70,4 @@ const methods = {
 
 
 let curMethod = process.argv[2].slice(0, process.argv[2].indexOf('-'))
-console.log(methods[curMethod]())
+methods[curMethod](process.argv[3])
